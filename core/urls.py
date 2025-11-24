f76bac77fbcca9cb9b1bc1211778cd660f6d2042
Anton <anton.averyan@gmail.com>
2025-11-24 22:49:08 +0100

    Wlkn' skellet and 2 muscules

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('how-it-works/', views.how_it_works, name='how_it_works'),
+    path('pricing/', views.pricing, name='pricing'),
     path('', views.landing, name='landing'),
     path('chat/', views.chat, name='chat'),
     path('positions/', views.positions, name='positions'),
--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+def how_it_works(request):
+    return render(request, 'core/how_it_works.html')
 
+def pricing(request):
+    return render(request, 'core/pricing.html')
 def landing(request):
     return render(request, 'core/landing.html')
 
